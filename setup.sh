7f579daaf2001d02b09614edad4a3ae58ca75f1a
Mark Solters <msolters@gmail.com>
2017-03-19 23:44:26 -0400

    setup.sh tested and working

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 # Some things will be downloaded, compiled, and installed so it may ask for
 # your password.
 
-HERE="$PWD"
-
-git clone https://github.com/nextthingco/CHIP-SDK
-cd "$HERE/CHIP-SDK"
-sh ./setup_ubuntu1404.sh
+if [ ! -d "$PWD/CHIP-SDK" ] ; then
+  git clone https://github.com/nextthingco/CHIP-SDK
+fi
+cd "$PWD/CHIP-SDK"
+bash setup_ubuntu1404.sh
