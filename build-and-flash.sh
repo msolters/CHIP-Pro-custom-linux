9955979b4257b622be06440ade1f13b4bea64f16
Mark Solters <msolters@gmail.com>
2017-03-22 15:35:08 -0400

    Fix typo in build-and-flash.sh

--- build-and-flash.sh
+++ build-and-flash.sh
@@ -3,7 +3,7 @@ CHIP_TOOLS_PATH="$PWD/CHIP-SDK/CHIP-tools"
 UBOOT_PATH="$PWD/CHIP-SDK/CHIP-buildroot/output/build/uboot-nextthing_2016.01_next"
 ROOTFS_PATH="$PWD/rootfs.tar"
 
-cd CHIP_TOOLS_PATH
+cd $CHIP_TOOLS_PATH
 rm -rf new-image
 sudo ./chip-create-nand-images.sh $UBOOT_PATH $ROOTFS_PATH new-image
 sudo chown -R $USER:$USER ./new-image
